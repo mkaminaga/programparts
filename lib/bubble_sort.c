7df1170d3d3e8746603d3d3e6c9912eeebe6ce3a
mkaminaga <mkaminaga2.718@gmai.com>
2016-02-04 20:12:33 +0900

    bubble sort added

---
+++ lib/bubble_sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/* sort given data.
+ * arg1: given data
+ * arg2: ascending(=1) and decending(=0)
+ * arg3: depth of index to be sorted */
+void bubble_sort(uint32_t data[], uint8_t way, uint32_t depth) {
+    uint8_t end_flag = 1;
+    uint32_t i = 0;
+    uint32_t temp = 0;
+
+    /* exception */
+    if (depth < 1) {
+        return;
+    }
+
+    end_flag = 0;
+
+    while (end_flag == 0) {
+        end_flag = 1;
+
+        if (way == 1) {
+            /* ascending order */
+            for (i = 0; i < depth - 1; i++) {
+                if(data[i] > data[i + 1]) {
+                    temp = data[i + 1];
+                    data[i + 1] = data[i];
+                    data[i] = temp;
+                    end_flag = 0;
+                }
+            }
+        } else {
+            /* decending order */
+            for (i = 0; i < depth - 1; i++) {
+                if(data[i] < data[i + 1]) {
+                    temp = data[i + 1];
+                    data[i + 1] = data[i];
+                    data[i] = temp;
+                    end_flag = 0;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ lib/bubble_sort.h
@@ -0,0 +1,5 @@
+/* sort given data.
+ * arg1: given data
+ * arg2: ascending(=1) and decending(=0)
+ * arg3: depth of index to be sorted */
+void bubble_sort(uint32_t data[], uint8_t way, uint32_t depth);
