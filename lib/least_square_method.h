19c900a5026ea6792eb330ae33fbac66e9165205
mkaminaga <mkaminaga2.718@gmai.com>
2016-02-04 20:05:48 +0900

    least square method OK.

---
+++ lib/least_square_method.h
@@ -0,0 +1,7 @@
+/* least_square(uint32_t, uint32_t, double, double, double)
+ * arg1: number od data points
+ * arg2: order of approximate curve
+ * arg3: x cordinates of plots
+ * arg4: y cordinates of plots
+ * arg5: solution */
+void least_square(uint32_t plot_num, uint32_t order, double x[], double y[], double a[]);
