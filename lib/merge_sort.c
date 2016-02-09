08da1e69770005ab2b1528134ae584830cfd2aa8
mkaminaga <mkaminaga2.718@gmai.com>
2016-02-09 21:49:24 +0900

    merge sort function complete.

--- lib/merge_sort.c
+++ lib/merge_sort.c
@@ -1,53 +1,73 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdint.h>
-#include <math.h>
 #include "merge_sort.h"
-#include "stack.h"
+
+/* void merge(uint32_t[], uint32_t, uint32_t)
+ * merge designate array segment
+ * arg1: pointer of the array
+ * arg2: start index of the segmet to be merged
+ * arg3: length of the segment to e merged */
+void merge(uint32_t a[], uint32_t start, uint32_t len);
+
+/* void msort(uint32_t[], uint32_t, uint32_t)
+ * divide and merge recrusivly.
+ * arg1: pointer to the array
+ * arg2: index of segment head
+ * arg3: length of segmant */
+void msort(uint32_t a[], uint32_t start, uint32_t length);
 
 /* void merge_sort(uint32_t[], uint32_t)
  * sort elements of array using merge sort.
- * arg1: array
+ * arg1: pointer of the array
  * arg2: length of array */
 void merge_sort(uint32_t a[], uint32_t length) {
+    msort(a, 0, length);
+}
 
-    stack_t stack;
+/* void msort(uint32_t[], uint32_t, uint32_t)
+ * divide and merge recrusivly.
+ * arg1: pointer to the array
+ * arg2: index of segment head
+ * arg3: length of segmant */
+void msort(uint32_t a[], uint32_t start, uint32_t length) {
+    uint32_t mid = 0;
 
-    uint32_t node = 1;
-    uint32_t flag = 0;
-    uint32_t node_max = 0;
-    int32_t i = 0;
-    uint32_t j = 0;
-
-    /* set depth */
-    node_max = (uint32_t) log2(length) + 1;
-    node_max = (uint32_t) pow(2, node_max);
-    printf("%u\n", node_max);
-
-    /* initialize stack */
-    init_stack(&stack);
-
-    /* push root node 1 */
-    push(&stack, 1);
-
-    while (!stack_is_empty(&stack)) {
-
-        /* get current node */
-        node = get_top_of_stack(&stack);
-        pop(&stack);
-
-        if (node < node_max) {
-            for (i = 1; i >= 0; i--) {
-                flag = 1;
-                for (j = 0; j <= stack.top; j++) {
-                    if (stack.box[j] == node * 2 + i) flag = 0;
-                }
-                if (flag == 1) push(&stack, node * 2 + i);
-            }
-        }
-
-        /* debug line */
-        printf("node:%u\n", node);
-        usleep(2E5);
+    if (length > 1) {
+        mid = length / 2;
+        msort(a, start, mid);
+        msort(a, start + mid, length - mid);
+        merge(a, start, length);
     }
+}
+
+/* void merge(uint32_t[], uint32_t, uint32_t)
+ * merge designate array segment
+ * arg1: pointer of the array
+ * arg2: start index of the segmet to be merged
+ * arg3: length of the segment to e merged */
+void merge(uint32_t a[], uint32_t start, uint32_t len) {
+    uint32_t b[len]; //temporaly array
+    uint32_t ptr1 = 0; //index: left leaf of segment
+    uint32_t ptr2 = 0; //index: right leaf of segment
+    uint32_t mid = 0; //middle of segment
+    uint32_t i = 0;
+
+    mid = len / 2;
+    ptr1 = start;
+    ptr2 = start + mid;
+
+    /* merge */
+    for (i = 0; i < len; i++) {
+        /* compere, then insert smaller data */
+        if (ptr2 == start + len || (ptr1 < start + mid && a[ptr1] <= a[ptr2])) {
+            b[i] = a[ptr1];
+            ptr1++;
+        } else {
+            b[i] = a[ptr2];
+            ptr2++;
+        }
+    }
+
+    /* copy */
+    for (i = 0; i < len; i++) a[start + i] = b[i];
 }
