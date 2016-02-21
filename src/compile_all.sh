b9a22b1bb21182649718ae7f0921277f62fa2c86
mkaminaga <mkaminaga2.718@gmail.com>
2016-02-21 14:40:34 +0900

    .out -> .exe

--- src/compile_all.sh
+++ src/compile_all.sh
@@ -2,6 +2,6 @@
 
 for i in `ls *.c`;\
 do\
-    gcc ${i} -o `echo ${i} | sed 's/\.c/\.out/g'`\
+    gcc ${i} -o `echo ${i} | sed 's/\.c/\.exe/g'`\
     && ls `echo ${i} | sed 's/\.c/\.out/g'`;\
 done
