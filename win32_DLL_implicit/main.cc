f7b92d21de5d37ffe010aaf4f630da09b82931f1
mkaminaga <mkaminaga2.718@gmail.com>
2019-07-02 12:34:10 +0900

    Implicit DLL test is added

---
+++ win32_DLL_implicit/main.cc
@@ -0,0 +1,27 @@
+//
+// @file main.cc
+// @brief Test for dynamic link library (loaded implicitly).
+// @author
+// @date 2019-07-02 10:28:02
+// Copyright 2019
+//
+#include "./test_DLL.h"
+
+int main(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+
+  int v = 10;
+
+  HelloWorld();
+
+  printf("v = %d\n", v);
+
+  v = Increment(v);
+  printf("Incremented, v = %d\n", v);
+
+  v = Decrement(v);
+  printf("Decremented, v = %d\n", v);
+
+  return 0;
+}
---
+++ win32_DLL_implicit/test_DLL.cc
@@ -0,0 +1,15 @@
+//
+// @file test_DLL.cc
+// @brief Test for dynamic link library.
+// @author Mamoru Kaminaga
+// @date 2019-07-02 10:13:52
+// Copyright 2019 Mamoru Kaminaga
+//
+#define DLLAPI extern "C" __declspec(dllexport)
+
+#include "./test_DLL.h"
+#include <stdio.h>
+
+DLLAPI void HelloWorld() { printf("Hello World!"); }
+DLLAPI int Increment(int v) { return v + 1; }
+DLLAPI int Decrement(int v) { return v + 1; }
