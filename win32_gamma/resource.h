b2b016aac1ceb9c5e3aa8d797839cac192bdc7d4
mkaminaga <mkaminaga2.718@gmail.com>
2019-10-09 20:29:12 +0900

    gamma is get

--- win32_gamma/resource.h
+++ win32_gamma/resource.h
@@ -5,5 +5,4 @@
 #define IDI_ICON1                               101
 #define IDD_DIALOG1                             103
 #define IDC_EDIT1                               40000
-#define IDSET                                   40001
 #define IDGET                                   40003
