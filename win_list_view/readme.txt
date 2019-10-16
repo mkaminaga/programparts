1f482dfb2384192d517f6b7866ab8d544d6f4721
mkaminaga <mkaminaga2.718@gmail.com>
2019-10-16 20:28:31 +0900

    test dialog editted

--- win_list_view/readme.txt
+++ win_list_view/readme.txt
@@ -2,3 +2,13 @@
 作成者 Mamoru Kaminaga
 
 ListViewコントロールのテストです。
+
+
+ResEditでリストビューコントロールが含まれるファイルを読み込もうとすると、エラーが出て読み込めない問題が生じている。
+以下のコントロールをリソースファイルに手動で追加。
+
+resource.rc
+CONTROL         "", IDC_LISTVIEW1, WC_LISTVIEW, WS_TABSTOP | WS_BORDER | LVS_ALIGNLEFT | LVS_ICON, 5, 5, 180, 240, WS_EX_LEFT
+
+resource.h
+#define IDC_IDC_LISTVIEW1                       40116
--- win_list_view/resource.h
+++ win_list_view/resource.h
@@ -4,9 +4,20 @@
 
 #define IDI_ICON1                               102
 #define IDD_DIALOG1                             104
-#define IDC_LISTVIEW1                           40000
+#define IDC_ROW                                 40000
 #define IDC_EDIT1                               40001
 #define IDLIST                                  40002
 #define IDDETAIL                                40003
 #define IDLICON                                 40004
 #define IDSICON                                 40005
+#define IDCLEAR                                 40006
+#define IDSETSIZE                               40007
+#define IDCLEARALL                              40008
+#define IDDELETEALL                             40009
+#define IDC_COLUMN                              40010
+#define IDGETITEM                               40011
+#define IDSETITEM                               40013
+#define IDDELETE                                40014
+#define IDGETALL                                40015
+#define IDC_INPUT                               40016
+#define IDC_LISTVIEW1                           40100
